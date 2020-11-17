75cbd3b4aa321143890ca2a0eb5575b60551bad2
Faiz <faiz.anwar4749@gmail.com>
2020-11-18 01:21:48 +0530

    [Faiz] Add

---
+++ UC1-CreateTable.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Address_Book
+(
+    First_Name varchar(40),
+    Last_Name varchar(40),
+    Address varchar(50),
+    City varchar(30),
+    State varchar(30),
+    Zip int,
+    Phone_Number varchar(20),
+    EmailID varchar(50)
+);
+select * from Address_Book;
